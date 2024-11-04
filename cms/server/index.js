7e8b64d1cf733b7644d3ee9c264c45252a25e17b
Gavin Yan <gavinyan10@gmail.com>
2024-11-04 17:11:23 -0500

    fetch content from frontend

--- cms/server/index.js
+++ cms/server/index.js
@@ -2,13 +2,13 @@ const express  = require('express')
 const {body, validationResult} = require('express-validator')
 const fs = require('fs');
 const bodyParser = require('body-parser')
+const cors = require("cors")
 
 const port = 3000
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
-var info = require('./info.json')
-var contact = require('./contacts.json')
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
