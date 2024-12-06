c19861633f23b73d3294dcf08619a46b3796a08b
Gavin Yan <gavinyan10@gmail.com>
2024-12-06 11:46:54 -0500

    ready for deployment

--- cms/server/app.js
+++ cms/server/app.js
@@ -1,8 +1,9 @@
 // index.js
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const mongoose = require('mongoose');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const port = 3000;
 const app = express();
@@ -11,115 +12,119 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+  })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
 // Define Mongoose Schemas and Models
 const contentSchema = new mongoose.Schema({
-    aboutMe: String,
-    languages: [String],
-    technologies: [String],
-    developerTools: [String]
+  aboutMe: String,
+  languages: [String],
+  technologies: [String],
+  developerTools: [String],
 });
-const Content = mongoose.model('Content', contentSchema);
+const Content = mongoose.model("Content", contentSchema);
 
 const contactSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    subject: String
+  firstName: String,
+  lastName: String,
+  subject: String,
 });
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 // Define Routes
 
 // Content Routes
-app.get('/contents', async (req, res) => {
-   try {
-        const contents = await Content.findOne(); // Fetch single document
-        res.status(200).json(contents);
-    } catch (error) {
-        res.status(400).json({ 'success': false });
+app.get("/contents", async (req, res) => {
+  try {
+    const contents = await Content.findOne(); // Fetch single document
+    res.status(200).json(contents);
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+});
+
+app.post(
+  "/contents",
+  body("aboutMe").notEmpty().isString(),
+  body("languages").notEmpty().isArray(),
+  body("technologies").notEmpty().isArray(),
+  body("developerTools").notEmpty().isArray(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("Validation errors:", errors.array()); // Log validation errors
+      return res.status(400).json({ error: errors.array() });
     }
-});
 
-app.post('/contents', 
-    body('aboutMe').notEmpty().isString(),
-    body('languages').notEmpty().isArray(),
-    body('technologies').notEmpty().isArray(),
-    body('developerTools').notEmpty().isArray(),
-    async (req, res) => {
-
-const errors = validationResult(req);
-if (!errors.isEmpty()) {
-   console.log("Validation errors:", errors.array());  // Log validation errors
-    return res.status(400).json({ error: errors.array() });
-}
-
-try {
-    await Content.findOneAndUpdate({}, req.body, {new: true, upsert: true })
-    res.status(200).json({ 'success': true });
-} catch (error) {
-   console.log("Error saving data:", error);  // Log save error
-    res.status(400).json({ 'success': false });
-}
-});
+    try {
+      await Content.findOneAndUpdate({}, req.body, { new: true, upsert: true });
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.log("Error saving data:", error); // Log save error
+      res.status(400).json({ success: false });
+    }
+  }
+);
 
 // Contact Routes
-app.get('/contacts', async (req, res) => {
-    try {
-        const contacts = await Contact.find();
-        res.status(200).json(contacts);
-    } catch (error) {
-        res.status(400).json({ 'success': false });
-    }
+app.get("/contacts", async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 });
 
-app.post('/contacts',
-         body('firstName').notEmpty().isString(),
-         body('lastName').notEmpty().isString(),
-         body('subject').notEmpty().isString(),
-         async (req, res) => {
-
+app.post(
+  "/contacts",
+  body("firstName").notEmpty().isString(),
+  body("lastName").notEmpty().isString(),
+  body("subject").notEmpty().isString(),
+  async (req, res) => {
     // Validate
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ error: "Validation failed" });
+      return res.status(400).json({ error: "Validation failed" });
     }
 
     try {
-        const newContact = new Contact(req.body);
-        const saved = await newContact.save();
-        res.status(200).json({ 'success': true, 'id': saved._id});
+      const newContact = new Contact(req.body);
+      const saved = await newContact.save();
+      res.status(200).json({ success: true, id: saved._id });
     } catch (error) {
-        res.status(400).json({ 'success': false });
+      res.status(400).json({ success: false });
     }
+  }
+);
+
+app.get("/contacts/:id", async (req, res) => {
+  try {
+    const contact = await Contact.findById(req.params.id).exec();
+    if (contact) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 });
 
-app.get('/contacts/:id', async (req, res) => {
-    try {
-        const contact = await Contact.findById(req.params.id).exec();
-        if (contact) {
-            res.status(200).json(contact);
-        } else {
-            res.status(404).json({ 'success': false });
-        }
-    } catch (error) {
-        res.status(400).json({ 'success': false });
-    }
-});
-
-app.delete('/contacts/:id', async (req, res) => {
-    try {
-        await Contact.findByIdAndDelete(req.params.id)
-        res.status(204).json({ 'success': true });
-    } catch (error) {
-        res.status(400).json({ 'success': false });
-    }
+app.delete("/contacts/:id", async (req, res) => {
+  try {
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(204).json({ success: true });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
--- website/script.js
+++ website/script.js
@@ -1,39 +1,41 @@
-async function loadContent(){
-    const res = await fetch("https://server-crimson-field-4080.fly.dev/contents")
-    const content = await res.json()
-    document.querySelector("#description").textContent = content.aboutMe
+const BACKEND_URL = "https://server-proud-grass-7453.fly.dev";
 
-    const languagesElement = document.querySelector("#language-list")
-    const technologiesElement = document.querySelector("#tech-list")
-    const developerToolsElement = document.querySelector("#devtools-list")
+async function loadContent() {
+  const res = await fetch(`${BACKEND_URL}/contents`);
+  const content = await res.json();
+  document.querySelector("#description").textContent = content.aboutMe;
 
-    fillLi(languagesElement, content.languages)
-    fillLi(technologiesElement, content.technologies)
-    fillLi(developerToolsElement, content.developerTools)
+  const languagesElement = document.querySelector("#language-list");
+  const technologiesElement = document.querySelector("#tech-list");
+  const developerToolsElement = document.querySelector("#devtools-list");
+
+  fillLi(languagesElement, content.languages);
+  fillLi(technologiesElement, content.technologies);
+  fillLi(developerToolsElement, content.developerTools);
 }
-function fillLi(ul, items){
-    items.forEach((item)=>{
-        li = document.createElement("li")
-        li.textContent = item
-        ul.appendChild(li)
-    })
+function fillLi(ul, items) {
+  items.forEach((item) => {
+    li = document.createElement("li");
+    li.textContent = item;
+    ul.appendChild(li);
+  });
 }
 
-loadContent()
+loadContent();
 
-function submitForm(){
-    const form = document.querySelector("#form")
-    const formData = new FormData(form)
-    fetch("https://server-cool-glade-3651.fly.dev/contacts", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            firstName: formData.get("firstName"),
-            lastName: formData.get("lastName"),
-            subject: formData.get("subject")
-        })
-    })
-    form.reset()
+function submitForm() {
+  const form = document.querySelector("#form");
+  const formData = new FormData(form);
+  fetch(`${BACKEND_URL}/contacts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      subject: formData.get("subject"),
+    }),
+  });
+  form.reset();
 }
